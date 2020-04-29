1580f762e2a629e4beebf842c7e45afdc77d5860
VishRosh001 <vishrosh001@gmail.com>
2020-04-29 17:22:42 +0100

    Add getCurrentLogger test

--- src/com/vishrosh/logger/tests/Main.java
+++ src/com/vishrosh/logger/tests/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		Logger.getLogger(Main.class).log(Level.MINOR, "Source", "This is a hello message!");
 		Logger.getLogger(Main.class).log("This is a hello message!");
+		Logger.getCurrentLogger().log("fdfd");
 	}
 
 }
